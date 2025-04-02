65f216f0c11bdf0eee526cd62c9f30596c08f48d
HERDEVLAN <herdevlanx@gmail.com>
2025-04-01 22:36:34 -0400

    funcionalidad de cambio de estado y filtrado para empleados

--- src/App.js
+++ src/App.js
@@ -6,26 +6,7 @@ import "./estilos/App.css"; // Importa los estilos del proyecto.
 import empleadosData from"./datos/empleados.json"; // Importa los datos de empleados desde un archivo JSON.
 import CmpEmpleados from"./componentes/CmpEmpleados"; // Importa el componente que mostrará la lista de empleados.
 
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactojoioi
-        </a>
-      </header>
-    </div>
-  );
-}*/
+
 
 class App extends Component{
 
@@ -87,11 +68,5 @@ cambiarEstadoEmpleado=(id) =>{
   
 
 }
-
-
-
-
-
-
 
 export default App;
